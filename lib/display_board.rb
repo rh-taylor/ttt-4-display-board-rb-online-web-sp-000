cb653251afafd3a756ff2a1da1f90e043252ce74
Robyn Taylor <rhtaylor0715@gmail.com>
2021-02-27 21:59:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,7 +10,7 @@ def display_board(board)
 end
 
 
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts "  #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
