91a3fa9953b0b6b06ad624dab4074935a2a1c697
Robyn Taylor <rhtaylor0715@gmail.com>
2021-02-27 22:03:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@ board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts " |   |   "
 end
